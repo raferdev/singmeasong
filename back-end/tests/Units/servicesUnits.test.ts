18bfc8502d715686cdf3ef866628b6af81c63125
Rafael <rafinha_fern@hotmail.com>
2022-07-28 12:49:54 -0300

    feat: unit test insert function

--- back-end/tests/Units/servicesUnits.test.ts
+++ back-end/tests/Units/servicesUnits.test.ts
@@ -1,8 +1,9 @@
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 import { recommendationService } from "../../src/services/recommendationsService.js";
 import Factory from "../Factory/index.js";
-import {jest} from "@jest/globals"
-describe("RECOMENDATIONS SERVICE TESTS",()=> {
+import {jest} from "@jest/globals";
+
+describe("RECOMENDATIONS SERVICE TESTS - INSERT FUNCTION",()=> {
 
     it("TEST INSERT FUNCTION, should throw error with duplicate data", async ()=>{
 
@@ -25,6 +26,17 @@ describe("RECOMENDATIONS SERVICE TESTS",()=> {
           }
 
         });
+    it("TEST INSERT FUNCTION, should call create function when have no duplicate data", async ()=>{
+
+          const obj = Factory.Recomends.video();
+          jest.spyOn(recommendationRepository, 'findByName').mockResolvedValueOnce(null);
+          jest.spyOn(recommendationRepository,"create").mockResolvedValueOnce();
+    
+          await recommendationService.insert(obj);
+
+          expect(recommendationRepository.create).toHaveBeenCalled();
+    
+            });
       });
 
 
