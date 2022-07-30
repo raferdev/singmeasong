d9f9b68cf37ec5612211ff6ee666d5dcebf33fe5
Rafael <rafinha_fern@hotmail.com>
2022-07-30 01:06:34 -0300

    feat: test unit get and get top, 100% coverage services tests

--- back-end/tests/Units/servicesUnits.test.ts
+++ back-end/tests/Units/servicesUnits.test.ts
@@ -7,12 +7,14 @@ describe("RECOMMENDATIONS SERVICE TESTS - INSERT FUNCTION", () => {
   it("TEST INSERT FUNCTION, should throw error with duplicate data", async () => {
     const obj = Factory.Recomends.video();
     const video = {
-        id: 1,
-        name: obj.name,
-        youtubeLink: obj.youtubeLink,
-        score: 12313,
-      };
-    jest.spyOn(recommendationRepository, "findByName").mockResolvedValueOnce(video);
+      id: 1,
+      name: obj.name,
+      youtubeLink: obj.youtubeLink,
+      score: 12313,
+    };
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockResolvedValueOnce(video);
     jest.spyOn(recommendationRepository, "create").mockResolvedValueOnce();
 
     try {
@@ -58,7 +60,6 @@ describe("RECOMMENDATIONS SERVICE TESTS - UPVOTE FUNCTION", () => {
   });
 
   it("TEST UPVOTE FUNCTION, should receive one wrong number input and throw ", async () => {
-
     jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
 
     try {
@@ -108,7 +109,7 @@ describe("RECOMMENDATIONS SERVICE TESTS - DOWNVOTE FUNCTION", () => {
     jest
       .spyOn(recommendationRepository, "updateScore")
       .mockResolvedValueOnce(video2);
-    jest.spyOn(recommendationRepository, "remove").mockResolvedValueOnce()
+    jest.spyOn(recommendationRepository, "remove").mockResolvedValueOnce();
 
     try {
       await recommendationService.downvote(1);
@@ -130,16 +131,21 @@ describe("RECOMMENDATIONS SERVICE TESTS - GET RANDOM FUNCTION", () => {
       score: 10,
     };
 
-    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((obj):any => {
-      if(obj.score === 10 && (obj.scoreFilter === 'gt' || obj.scoreFilter === 'lte')){
-        return [video]
-      }
-      throw {type: 'fail obj validation'}
-    });
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockImplementationOnce((obj): any => {
+        if (
+          obj.score === 10 &&
+          (obj.scoreFilter === "gt" || obj.scoreFilter === "lte")
+        ) {
+          return [video];
+        }
+        throw { type: "fail obj validation" };
+      });
 
     try {
       const result = await recommendationService.getRandom();
-      expect(result).toEqual(video)
+      expect(result).toEqual(video);
     } catch (e) {
       console.log(e);
       expect(e).toBeUndefined();
@@ -155,21 +161,60 @@ describe("RECOMMENDATIONS SERVICE TESTS - GET RANDOM FUNCTION", () => {
       score: 10,
     };
 
-    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((obj):any => {
-      if(obj.score === 10 && (obj.scoreFilter === 'gt' || obj.scoreFilter === 'lte')){
-        return []
-      }
-      throw {type: 'fail obj validation'}
-    });
-    jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce([video])
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockImplementationOnce((obj): any => {
+        if (
+          obj.score === 10 &&
+          (obj.scoreFilter === "gt" || obj.scoreFilter === "lte")
+        ) {
+          return [];
+        }
+        throw { type: "fail obj validation" };
+      });
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValueOnce([video]);
     try {
       const result = await recommendationService.getRandom();
-      expect(result).toEqual(video)
+      expect(result).toEqual(video);
     } catch (e) {
       console.log(e);
       expect(e).toBeUndefined();
     }
   });
+
+  it("Should throw error when findAll with no constraints and findAll with filter are null", async () => {
+    const obj = Factory.Recomends.video();
+    const video = {
+      id: 5,
+      name: obj.name,
+      youtubeLink: obj.youtubeLink,
+      score: 10,
+    };
+
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockImplementationOnce((obj): any => {
+        if (
+          obj.score === 10 &&
+          (obj.scoreFilter === "gt" || obj.scoreFilter === "lte")
+        ) {
+          return [];
+        }
+        throw { type: "fail obj validation" };
+      });
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValueOnce([]);
+    try {
+      const result = await recommendationService.getRandom();
+      expect(result).toBeUndefined();
+    } catch (e) {
+      expect(e).toBeDefined();
+    }
+  });
+
   it("Should receive onde number greater than 0.7 and filter by 'lte'", async () => {
     const obj = Factory.Recomends.video();
     const video = {
@@ -180,22 +225,23 @@ describe("RECOMMENDATIONS SERVICE TESTS - GET RANDOM FUNCTION", () => {
     };
     jest.spyOn(Math, "random").mockReturnValueOnce(0.8);
 
-    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((obj):any => {
-      if(obj.score === 10 && obj.scoreFilter === 'lte') {
-
-        return [video]
-
-      }
-      throw {type: 'fail obj validation'}
-    });
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockImplementationOnce((obj): any => {
+        if (obj.score === 10 && obj.scoreFilter === "lte") {
+          return [video];
+        }
+        throw { type: "fail obj validation" };
+      });
     try {
       const result = await recommendationService.getRandom();
-      expect(result).toEqual(video)
+      expect(result).toEqual(video);
     } catch (e) {
       console.log(e);
       expect(e).toBeUndefined();
     }
   });
+
   it("Should receive onde number less than 0.7 and filter by 'gt'", async () => {
     const obj = Factory.Recomends.video();
     const video = {
@@ -206,17 +252,18 @@ describe("RECOMMENDATIONS SERVICE TESTS - GET RANDOM FUNCTION", () => {
     };
     jest.spyOn(Math, "random").mockReturnValueOnce(0.6);
 
-    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((obj):any => {
-      if(obj.score === 10 && obj.scoreFilter === 'gt') {
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockImplementationOnce((obj): any => {
+        if (obj.score === 10 && obj.scoreFilter === "gt") {
+          return [video];
+        }
+        throw { type: "fail obj validation" };
+      });
 
-        return [video]
-
-      }
-      throw {type: 'fail obj validation'}
-    });
     try {
       const result = await recommendationService.getRandom();
-      expect(result).toEqual(video)
+      expect(result).toEqual(video);
     } catch (e) {
       console.log(e);
       expect(e).toBeUndefined();
@@ -233,21 +280,75 @@ describe("RECOMMENDATIONS SERVICE TESTS - GET RANDOM FUNCTION", () => {
 
     jest.spyOn(Math, "random").mockReturnValueOnce(0.7);
 
-    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((obj):any => {
-      if(obj.score === 10 && obj.scoreFilter === 'lte') {
-
-        return [video]
-
-      }
-      throw {type: 'fail obj validation'}
-    });
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockImplementationOnce((obj): any => {
+        if (obj.score === 10 && obj.scoreFilter === "lte") {
+          return [video];
+        }
+        throw { type: "fail obj validation" };
+      });
     try {
       const result = await recommendationService.getRandom();
-      expect(result).toEqual(video)
+      expect(result).toEqual(video);
     } catch (e) {
       console.log(e);
       expect(e).toBeUndefined();
     }
   });
-  
+});
+
+describe("RECOMMENDATIONS SERVICE TESTS - GET FUNCTION", () => {
+  it("Should call repositorie with amount parameter", async () => {
+    const obj = Factory.Recomends.video();
+    const video = {
+      id: 4,
+      name: obj.name,
+      youtubeLink: obj.youtubeLink,
+      score: 10,
+    };
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValueOnce([video]);
+    try {
+      const result = await recommendationService.get();
+      expect(recommendationRepository.findAll).toBeCalled();
+      expect(result[0]).toEqual(video);
+    } catch (e) {
+      console.log(e);
+      expect(e).toBeUndefined();
+    }
+  });
+});
+
+describe("RECOMMENDATIONS SERVICE TESTS - GET TOP FUNCTION", ()=> {
+  it("Should call 'getAmountByScore' repository with amount number", async ()=> {
+      const amount = 10;
+      const obj = Factory.Recomends.video();
+      const video = {
+        id: 4,
+        name: obj.name,
+        youtubeLink: obj.youtubeLink,
+        score: 10,
+      };
+
+      jest
+      .spyOn(recommendationRepository, "getAmountByScore")
+      .mockImplementationOnce((amount):any => {
+        if(amount===10) {
+          return [video]
+        }
+        throw {type:"Invalid amount call"}
+      });
+
+    try {
+      const result = await recommendationService.getTop(amount);
+      expect(recommendationRepository.getAmountByScore).toBeCalled();
+      expect(result[0]).toEqual(video);
+    } catch (e) {
+      console.log(e);
+      expect(e).toBeUndefined();
+    }
+
+  });
 });
